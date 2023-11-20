9ced385d0d9b6ae5a94d8e03047c89d69b061693
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-20 21:15:42 +0400

    add RussianName

---
+++ FakePerson.lib/Models/Names/RussianName.cs
@@ -0,0 +1,8 @@
+﻿namespace FakePerson.lib.Models.Names;
+
+public record RussianName : AbstractName
+{
+    public required string LastName { get; init; }
+
+    public string? Patronymic { get; init; }
+}
