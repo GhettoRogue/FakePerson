964c3be3940771f9d97847e07b6638d2a4be07fa
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-21 11:45:42 +0400

    add extension class

---
+++ FakePerson.lib/RandomExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace FakePerson.lib;
+
+public static class RandomExtensions
+{
+    public static string NextString(this Random random, IEnumerable<string> source)
+    {
+        var list = source.ToList();
+        var i = random.Next(0, list.Count);
+        
+        return list[i];
+    }
+}
