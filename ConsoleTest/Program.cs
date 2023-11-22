ef7ba64297064ddf14a6e8e13250ab6683c180ed
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-22 22:06:12 +0400

    init console test proj

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,9 @@
+﻿using FakePerson.lib.Factories;
+using FakePerson.lib.Factories.NameFactories;
+
+
+var nameFactory = new RandomAbstractNameFactory(new RandomRussianNameFactory());
+var personBuilder = new PersonBuilder(nameFactory.Create());
+var random = new Random();
+
+var person = personBuilder.
