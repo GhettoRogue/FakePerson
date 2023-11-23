46508802735ccf4fd54b265dcb9ed702f73941cd
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-23 18:40:14 +0400

    update test console

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,9 +1,25 @@
-﻿using FakePerson.lib.Factories;
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using FakePerson.lib.Factories;
 using FakePerson.lib.Factories.NameFactories;
+using FakePerson.lib.Randomizers;
 
 
 var nameFactory = new RandomAbstractNameFactory(new RandomRussianNameFactory());
 var personBuilder = new PersonBuilder(nameFactory.Create());
 var random = new Random();
 
-var person = personBuilder.
+var person = personBuilder
+    .SetAge(random.NextAge())
+    .SetEmail("person@email.mail")
+    .Build();
+
+var jsonOptions = new JsonSerializerOptions()
+{
+    IncludeFields = true,
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+};
+var json = JsonSerializer.Serialize(person, jsonOptions);
+Console.WriteLine(json);
+
+Console.WriteLine(person);
